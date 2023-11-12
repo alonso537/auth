d0c28276d010511b2f6213d1df636dd84a9b849e
alonso537 <adrianaalonso002mail.com>
2023-11-12 12:03:21 -0600

    authService

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -11,18 +11,18 @@ export class RegisterUserDto {
   static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
     const { name, email, password } = object;
 
-    if (!name) return ["Missing name", undefined];
+    if (!name) return ["Missing name"];
 
-    if (!email) return ["Missing email", undefined];
+    if (!email) return ["Missing email"];
 
     if (!regularExps.email.test(email)) {
-      return ["Invalid email", undefined];
+      return ["Invalid email"];
     }
 
-    if (!password) return ["Missing password", undefined];
+    if (!password) return ["Missing password"];
 
     if (password.length < 6) {
-      return ["Password must be at least 6 characters", undefined];
+      return ["Password must be at least 6 characters"];
     }
 
     return [undefined, new RegisterUserDto(name, email, password)];
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
+import { RegisterUserDto } from "../../domain";
+import { AuthService } from "../services/auth.service";
 
 
 export class AuthController {
     //DI
-    constructor() {}
+    constructor(
+        public readonly authService: AuthService
+    ) {}
 
     
 
     register = (req: Request, res: Response) => {
 
-        res.json('Register User')
+        const [error, registerUserDto] = RegisterUserDto.create(req.body)
+
+        if (error) {
+            return res.status(400).json({ error })
+        }
+
+        this.authService.registerUser(registerUserDto!)
+            .then(user => {
+                res.json(user)
+            })
+            .catch(error => {
+                res.status(error.status).json({ error: error.message })
+            })
+
+        // res.json(registerUserDto)
     }
 
     login = (req: Request, res: Response) => {
