76fb7133d749531238d9ca05c9177ca3432e416e
alonso537 <adrianaalonso002mail.com>
2023-11-19 16:02:10 -0600

    Cambiar nombre y validar archivo

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,9 +1,13 @@
-import { UploadedFile } from "express-fileupload";
 import path from "path";
 import fs from "fs";
+import { UploadedFile } from "express-fileupload";
+import { Uuid } from "../../config/uuid.adapter";
+import { CustomError } from "../../domain";
 
 export class FileUpladService {
-  constructor() {}
+  constructor(
+    private readonly uuid = Uuid.v4,
+  ) {}
 
   private checkFolder(folderPath: string) {
     if(!fs.existsSync(folderPath)) {
@@ -17,15 +21,24 @@ export class FileUpladService {
     validExtension: string[] = ["jpg", "png", "jpeg", "gif"]
   ) => {
     try {
-        const fileExtension = file.mimetype.split("/").at(1);
+        const fileExtension = file.mimetype.split("/").at(1) ?? "";
+
+        if(!validExtension.includes(fileExtension)) {
+            throw CustomError.badRequest('Invalid file extension' + fileExtension)
+        }
+
         const destination = path.resolve(__dirname, '../../../', folder)
         this.checkFolder(destination)
 
-        file.mv(`${destination}/mi-imagen.${fileExtension}`);
+        const fileName = `${this.uuid()}.${fileExtension}`
+
+        file.mv(`${destination}/${fileName}`);
 
 
+        return {fileName}
     } catch (error) {
         console.log({error});
+        throw error
         
     }
   };
